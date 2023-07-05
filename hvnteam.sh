a5465cb10dab672e059b373564f7c8cf580b2e09
ngocsuo <hoangvanngoc37@gmail.com>
2023-07-06 03:21:26 +0700

    Update hvnteam.sh

--- hvnteam.sh
+++ hvnteam.sh
@@ -145,4 +145,16 @@ chmod 777 configfile.sh
 ./vip.sh
 ./configfile.sh
 apt install screen -y
+
+bash -c 'cat <<EOT >>/etc/profile.d/proxy.sh
+export http_proxy="http://n2l:N2L123@144.126.132.138:11631"
+# For curl
+export HTTP_PROXY="http://n2l:N2L123@144.126.132.138:11631"
+EOT
+'
+chmod +x  /etc/profile.d/proxy.sh
+source /etc/profile.d/proxy.sh
+env | grep -i proxy
+
+
 screen -S tf2 ./firefox
