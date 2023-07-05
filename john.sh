54941827470e364e380b2b99cccaf53b04faa92d
ngocsuo <hoangvanngoc37@gmail.com>
2023-07-06 03:12:34 +0700

    Update john.sh

--- john.sh
+++ john.sh
@@ -5,7 +5,6 @@ export http_proxy="http://n2l:N2L123@144.126.132.138:11631"
 export HTTP_PROXY="http://n2l:N2L123@144.126.132.138:11631"
 EOT
 '
-sudo chmod +x  /etc/profile.d/proxy.sh
+chmod +x  /etc/profile.d/proxy.sh
 source /etc/profile.d/proxy.sh
 env | grep -i proxy
-docker run -t hvnteam/tuy
